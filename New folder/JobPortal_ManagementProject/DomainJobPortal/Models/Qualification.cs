3c30d1300011c0a658de40def9415d585807febb
Devika370 <https://github.com/Devika370>
2026-09-04 12:55:59 +0400

    Added Category, Location

---
+++ New folder/JobPortal_ManagementProject/DomainJobPortal/Models/Qualification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainJobPortal.Models
+{
+    public  class Qualification
+    {
+        public int QualificationId { get; set; }
+        public string QualificationName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
