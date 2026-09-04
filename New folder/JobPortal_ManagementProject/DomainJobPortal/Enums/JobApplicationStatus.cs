292acb51f645f410292db0aa7b0f40d38e55f1af
Devika370 <https://github.com/Devika370>
2026-09-04 10:38:46 +0400

    Added Enums, AuthUser Model

---
+++ New folder/JobPortal_ManagementProject/DomainJobPortal/Enums/JobApplicationStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainJobPortal.Enums
+{
+    public enum JobApplicationStatus
+    {
+        Applied,
+        UnderReview,
+        ShortListed,
+        Interview,
+        Hired,
+        Rejected
+    }
+}
