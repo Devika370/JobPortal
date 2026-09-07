c17b4cfc7ddb4ffc86a688d30c98e55931766289
ameerasabiq2 <ameerasabiq2@gmail.com>
2026-09-07 12:45:35 +0400

    JobType added

---
+++ New folder/JobPortal_ManagementProject/DomainJobPortal/Models/JobType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainJobPortal.Models
+{
+    public class JobType
+    {
+        public int JobTypeId { get; set; }
+        public string JobTypeName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
