13b7c8bd29c5e9c16d6b865b7a6dacd464b35787
ameerasabiq2 <ameerasabiq2@gmail.com>
2026-09-08 17:38:50 +0400

    JobType added

---
+++ New folder/JobPortal_ManagementProject/DomainJobPortal/Models/Skill.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainJobPortal.Models
+{
+    public class Skill
+    {
+        public int SkillId { get; set; }
+        public string SkillName { get; set; } = string.Empty;
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
